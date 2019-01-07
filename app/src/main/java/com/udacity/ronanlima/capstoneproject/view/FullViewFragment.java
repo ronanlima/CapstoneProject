6e20f7fcde8e8ff3ad37cda4e1b89d90306ecc33
Ronan Sales Lima <ronan.saleslima@gmail.com>
2019-01-07 13:40:29 -0200

    - Minha alteração para funcionar com leanback

--- app/src/main/java/com/udacity/ronanlima/capstoneproject/view/FullViewFragment.java
+++ app/src/main/java/com/udacity/ronanlima/capstoneproject/view/FullViewFragment.java
@@ -138,7 +138,7 @@ public class FullViewFragment extends Fragment implements Runnable {
 
             if (isImmersiveAvailable()) {
                 flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION/** | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY*/;
             }
 
             activity.getWindow().getDecorView().setSystemUiVisibility(flags);
