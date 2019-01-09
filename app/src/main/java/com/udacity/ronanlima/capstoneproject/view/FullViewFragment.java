e5facb7fcd3683c2fff0a21a70264c9fe01aa4be
Ronan Sales Lima <ronan.saleslima@gmail.com>
2019-01-09 17:16:21 -0200

    - Inclusão da lib otaliastudios:zoomlayout

--- app/src/main/java/com/udacity/ronanlima/capstoneproject/view/FullViewFragment.java
+++ app/src/main/java/com/udacity/ronanlima/capstoneproject/view/FullViewFragment.java
@@ -1,21 +1,15 @@
 package com.udacity.ronanlima.capstoneproject.view;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.widget.ImageView;
 
-import com.ortiz.touchview.TouchImageView;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.RequestCreator;
 import com.udacity.ronanlima.capstoneproject.AppExecutors;
@@ -33,26 +27,17 @@ import butterknife.ButterKnife;
  * @author ronanlima
  * @since janeiro/2019
  */
-public class FullViewFragment extends Fragment implements Runnable {
+public class FullViewFragment extends Fragment {
     public static final String TAG = FullViewFragment.class.getSimpleName().toUpperCase();
 
     @BindView(R.id.iv_full_view)
-    TouchImageView ivFullView;
-    private Handler _handler;
-    private ScaleGestureDetector mScaleGestureDetector;
+    ImageView ivFullView;
     private float mScaleFactor = 1.0f;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        _handler = new Handler();
-    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_full_view, container, false);
-        mScaleGestureDetector = new ScaleGestureDetector(getActivity(), new ScaleListener());
         ButterKnife.bind(this, view);
         final String uriImage = getArguments().getString(GalleryFragment.ARGUMENT_URI_IMAGE);
         AppExecutors.getInstance().getDiskIO().execute(new Runnable() {
@@ -73,102 +58,8 @@ public class FullViewFragment extends Fragment implements Runnable {
                 }
             }
         });
-//        setFullscreen();
-//        if (Build.VERSION.SDK_INT > 10) {
-//            registerSystemUiVisibility();
-//        }
 
         return view;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (Build.VERSION.SDK_INT > 10) {
-            unregisterSystemUiVisibility();
-        }
-        exitFullscreen(getActivity());
-    }
-
-    public static boolean isImmersiveAvailable() {
-        return android.os.Build.VERSION.SDK_INT >= 19;
-    }
-
-    public void onWindowFocusChanged(boolean hasFocus) {
-        if (hasFocus) {
-            _handler.removeCallbacks(this);
-            _handler.postDelayed(this, 300);
-        } else {
-            _handler.removeCallbacks(this);
-        }
-    }
-
-    public void onKeyDown(int keyCode) {
-        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP)) {
-            _handler.removeCallbacks(this);
-            _handler.postDelayed(this, 500);
-        }
-    }
-
-    @Override
-    public void onStop() {
-        _handler.removeCallbacks(this);
-        super.onStop();
-    }
-
-    @Override
-    public void run() {
-        setFullscreen();
-    }
-
-    public void setFullscreen() {
-        setFullscreen(getActivity());
-    }
-
-    public void setFullscreen(Activity activity) {
-        if (Build.VERSION.SDK_INT > 10) {
-            int flags = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_FULLSCREEN;
-
-            if (isImmersiveAvailable()) {
-                flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION/** | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY*/;
-            }
-
-            activity.getWindow().getDecorView().setSystemUiVisibility(flags);
-        } else {
-            activity.getWindow()
-                    .setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
-    }
-
-    public void exitFullscreen(Activity activity) {
-        if (Build.VERSION.SDK_INT > 10) {
-            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-        } else {
-            activity.getWindow()
-                    .setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
-                            WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void registerSystemUiVisibility() {
-        final View decorView = getActivity().getWindow().getDecorView();
-        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                    setFullscreen();
-                }
-            }
-        });
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void unregisterSystemUiVisibility() {
-        final View decorView = getActivity().getWindow().getDecorView();
-        decorView.setOnSystemUiVisibilityChangeListener(null);
     }
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
