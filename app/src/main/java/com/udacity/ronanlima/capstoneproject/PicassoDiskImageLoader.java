b969b85f7bfec15c5f4b4f6dff796d46bb2eec72
Ronan Sales Lima <ronan.saleslima@gmail.com>
2019-01-09 20:40:35 -0200

    - Inclusão da lib veinhorn.scrollgalleryview e remoção das 'com.github.MikeOrtiz:TouchImageView:1.4.0' e 'com.otaliastudios:zoomlayout:1.4.0'; - Criação da classe PicassoDiskImageLoader extendendo de MediaLoader, para ser usada pela lib scrollgalleryview.

---
+++ app/src/main/java/com/udacity/ronanlima/capstoneproject/PicassoDiskImageLoader.java
@@ -0,0 +1,71 @@
+package com.udacity.ronanlima.capstoneproject;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+import com.veinhorn.scrollgalleryview.loader.MediaLoader;
+
+import java.io.File;
+
+public class PicassoDiskImageLoader implements MediaLoader {
+    private String uri;
+    private Integer thumbnailWidth;
+    private Integer thumbnailHeight;
+
+    public PicassoDiskImageLoader(String uri) {
+        this.uri = uri;
+    }
+
+    public PicassoDiskImageLoader(String uri, Integer thumbnailWidth, Integer thumbnailHeight) {
+        this.uri = uri;
+        this.thumbnailWidth = thumbnailWidth;
+        this.thumbnailHeight = thumbnailHeight;
+    }
+
+    @Override
+    public boolean isImage() {
+        return true;
+    }
+
+    @Override
+    public void loadMedia(Context context, final ImageView imageView, final MediaLoader.SuccessCallback callback) {
+        File file = new File(uri);
+        Picasso.get()
+                .load(file)
+                .placeholder(R.drawable.placeholder_image)
+                .into(imageView, new ImageCallback(callback));
+    }
+
+    @Override
+    public void loadThumbnail(Context context, final ImageView thumbnailView, final MediaLoader.SuccessCallback callback) {
+        File file = new File(uri);
+        Picasso.get()
+                .load(file)
+                .resize(thumbnailWidth == null ? 100 : thumbnailWidth,
+                        thumbnailHeight == null ? 100 : thumbnailHeight)
+                .placeholder(R.drawable.placeholder_image)
+                .centerInside()
+                .into(thumbnailView, new ImageCallback(callback));
+    }
+
+    private static class ImageCallback implements Callback {
+        private final MediaLoader.SuccessCallback callback;
+
+        public ImageCallback(SuccessCallback callback) {
+            this.callback = callback;
+        }
+
+        @Override
+        public void onSuccess() {
+            callback.onSuccess();
+        }
+
+        @Override
+        public void onError(Exception e) {
+
+        }
+
+    }
+}
--- app/src/main/java/com/udacity/ronanlima/capstoneproject/view/FullViewFragment.java
+++ app/src/main/java/com/udacity/ronanlima/capstoneproject/view/FullViewFragment.java
@@ -1,22 +1,23 @@
 package com.udacity.ronanlima.capstoneproject.view;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.RequestCreator;
-import com.udacity.ronanlima.capstoneproject.AppExecutors;
+import com.udacity.ronanlima.capstoneproject.PicassoDiskImageLoader;
 import com.udacity.ronanlima.capstoneproject.R;
+import com.udacity.ronanlima.capstoneproject.data.Image;
+import com.udacity.ronanlima.capstoneproject.viewmodel.FirebaseViewModel;
+import com.veinhorn.scrollgalleryview.MediaInfo;
+import com.veinhorn.scrollgalleryview.ScrollGalleryView;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,47 +31,31 @@ import butterknife.ButterKnife;
 public class FullViewFragment extends Fragment {
     public static final String TAG = FullViewFragment.class.getSimpleName().toUpperCase();
 
-    @BindView(R.id.iv_full_view)
-    ImageView ivFullView;
-    private float mScaleFactor = 1.0f;
+    @BindView(R.id.scroll_gallery_view)
+    ScrollGalleryView scrollGalleryView;
+    private List<Image> images;
+    private FirebaseViewModel viewModel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_full_view, container, false);
         ButterKnife.bind(this, view);
-        final String uriImage = getArguments().getString(GalleryFragment.ARGUMENT_URI_IMAGE);
-        AppExecutors.getInstance().getDiskIO().execute(new Runnable() {
-            @Override
-            public void run() {
-                File file = new File(uriImage);
-                final RequestCreator load = Picasso.get().load(file);
-                try {
-                    load.get();
-                    AppExecutors.getInstance().getMainThread().execute(new Runnable() {
-                        @Override
-                        public void run() {
-                            load.into(ivFullView);
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        viewModel = ViewModelProviders.of(getActivity()).get(FirebaseViewModel.class);
+        images = viewModel.getDataImage().getValue();
+
+        final List<MediaInfo> infos = new ArrayList<>(FullViewFragment.this.images.size());
+        for (Image img : images) {
+            infos.add(MediaInfo.mediaLoader(new PicassoDiskImageLoader(img.getUriImagem())));
+        }
+
+        scrollGalleryView
+                .setThumbnailSize(100)
+                .setZoom(true)
+                .setFragmentManager(getFragmentManager())
+                .addMedia(infos);
 
         return view;
-    }
-
-    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            mScaleFactor *= detector.getScaleFactor();
-            mScaleFactor = Math.max(1f, Math.min(mScaleFactor, 5.0f));
-            ivFullView.setScaleX(mScaleFactor);
-            ivFullView.setScaleY(mScaleFactor);
-            return true;
-        }
     }
 
 }
